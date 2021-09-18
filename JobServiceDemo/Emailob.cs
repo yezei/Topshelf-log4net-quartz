e98da1aacd8d4f455207dd473be5780e07fd4fc2
yezei <599067663@qq.com>
2021-09-18 18:28:08 +0800

    upload project

---
+++ JobServiceDemo/Emailob.cs
@@ -0,0 +1,51 @@
+﻿using log4net;
+using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailServices.Common
+{
+    [PersistJobDataAfterExecution]//保存执行状态
+    [DisallowConcurrentExecution]//不允许并发执行
+    public class Emailob : IJob
+    {
+        //日志
+        private static readonly ILog logger = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        async Task IJob.Execute(IJobExecutionContext context)
+        {
+            logger.Debug("开始发送邮件");
+            try
+            {
+                await Task.Run(() => SendEmail());
+            }
+            catch (Exception ex)
+            {
+                await Task.Run(() => logger.Error(ex.Message));
+                throw ex;
+            }
+            logger.Debug("邮件发送结束");
+        }
+
+        public void SendEmail()
+        {
+            System.Net.Mail.SmtpClient client = new System.Net.Mail.SmtpClient();
+            client.Host = "smtp.163.com";
+            client.UseDefaultCredentials = true;
+            client.DeliveryMethod = System.Net.Mail.SmtpDeliveryMethod.Network;
+            client.Credentials = new System.Net.NetworkCredential("huishanmail@163.com", "huishan0731");
+            System.Net.Mail.MailMessage Message = new System.Net.Mail.MailMessage();
+            Message.From = new System.Net.Mail.MailAddress("huishanmail@163.com");
+            Message.To.Add("599067663@qq.com");
+            Message.Subject = "测试标体";
+            Message.Body = "测试邮件体";
+            Message.SubjectEncoding = System.Text.Encoding.UTF8;
+            Message.BodyEncoding = System.Text.Encoding.UTF8;
+            Message.Priority = System.Net.Mail.MailPriority.High;
+            Message.IsBodyHtml = true;
+            client.Send(Message);
+        }
+    }
+}
---
+++ JobServiceDemo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using Topshelf;
+
+namespace JobServiceDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+            log4net.Config.XmlConfigurator.ConfigureAndWatch(new FileInfo(AppDomain.CurrentDomain.BaseDirectory + "log4net.config"));
+            HostFactory.Run(x =>
+            {
+                x.UseLog4Net();
+
+                x.Service<ServiceRunner>(s => {
+                    s.ConstructUsing(name => new ServiceRunner());
+                    s.WhenStarted((tc, hc) => tc.Start(hc));
+                    s.WhenStopped((tc, hc) => tc.Stop(hc));
+                    s.WhenContinued((tc, hc) => tc.Continue(hc));
+                    s.WhenPaused((tc, hc) => tc.Pause(hc));
+                });
+
+                x.RunAsLocalService();
+                x.StartAutomaticallyDelayed();
+
+                x.SetDescription("用于系统定时任务计划执行");
+                x.SetDisplayName("JobService");
+                x.SetServiceName("JobService");
+
+                x.EnablePauseAndContinue();
+            });
+        }
+    }
+}
